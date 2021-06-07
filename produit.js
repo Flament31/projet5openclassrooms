fdffe4974d405894a5b37e5aef987519a3f642f3
Baudouin Flament <baudouinflament@gmail.com>
2021-06-07 23:32:24 +0200

    modification de produit

--- produit.js
+++ produit.js
@@ -24,103 +24,91 @@ function makeRequest (method, url) {
     };
     xhr.send();
   });
-}
+};
 
-const urlApi = "https://oc-p5-api.herokuapp.com/api/furniture/";
-const searchParams = new URLSearchParams(window.location.search);
-const id = searchParams.get('id');
-const urlApiId = urlApi + "/"+id;
-console.log(id);
+function color(colors){
+    let html = '<option value="">Choisir la couleur</option>'
+        for (i = 0; i < colors.length; i++){
+            html+=`            
+                <option value="${colors[i]}">${colors[i]}</option>`;
+        };
+    return html;
+};
 
-makeRequest('GET', 'https://oc-p5-api.herokuapp.com/api/furniture/5be9cc611c9d440000c1421e')
-.then(function (datums) {
-    let produit = datums;
-    const getProduits = async function() {
-        try {
-            let response = await fetch('https://oc-p5-api.herokuapp.com/api/furniture/' + id);        
-            if (response.ok) {
-                let produit = await response.json();
-                console.log(produit);
+function produitHtml(produit){
+        
+        document.getElementById("produit").innerHTML +=`
+        <div class="col-12 col-md-6 card">
+            <img class=”card-img-top” src="${produit.imageUrl}">
+            <div class="card-body">
+                <h2 class="card-title">${produit.name}</h2>
+                <p class="card-text">${produit.price / 100 + "€"}</p>
+            </div>
+        </div> 
 
-                let idProduit = document.getElementById("produit");            
-                let colors = produit.varnish;
-            
+        <div class="col-12 col-md-6 card">
+            <div class="card-body">
+                <p class="card-text">
+                    ${produit.description}
+                </p>
+                
+                <div class="form-group">
+                    <select id="selection" class="form-control">
+                        ${color(produit.varnish)}
+                    </select>
+                </div>
 
-                idProduit.innerHTML +=`
-                <div class="col-12 col-md-6 card">
-                    <img class=”card-img-top” src="${produit.imageUrl}">
-                    <div class="card-body">
-                        <h2 class="card-title">${produit.name}</h2>
-                        <p class="card-text">${produit.price / 100 + "€"}</p>
-                    </div>
-                </div> 
+                <button id="btn-pannier" type="button" class="btn btn-primary">
+                    Ajouter au pannier
+                </button>
+            </div>                
+        </div>`;
+}; 
 
-                <div class="col-12 col-md-6 card">
-                    <div class="card-body">
-                        <p class="card-text">
-                            ${produit.description}
-                        </p>
-                        
-                        <div class="form-group">
-                            <select id="selection" class="form-control">
-                                <option value="">Choisir la couleur</option>
-                                
-                            </select>
-                        </div>
+function pushProduit(produitColor, produit,){
+    if (window.confirm(produit.name + " " + produitColor + ' a bien été ajouté. Souhaitez vous consulter votre panier ?')) { 
+        window.location.href = "panier.html";
+    } else {
+        window.location.href = "index.html";
+    }
+};
 
-                        <button id="btn-pannier" type="button" class="btn btn-primary">
-                            Ajouter au pannier
-                        </button>
-                    </div>                
-                </div>`;
-                                                 
-                let optionColor = document.querySelector("select");
-                for (i = 0; i < colors.length; i++){
-                    optionColor.innerHTML +=`
-                        <option value="${colors[i]}">${colors[i]}</option>`;
-                };
-                 
-             
-                let select = document.querySelector("select");
-                let value = document.querySelector("option");
+function choosen(produit, storedProduits, produitsChoosen){
+    document.querySelector(".btn-primary").addEventListener("click", function (event) {
+        event.preventDefault();
 
+        if(storedProduits) {
+            pushProduit(produit, produitsChoosen);
+        } else {
+            storedProduits = [];
+            pushProduit(produit, produitsChoosen);
+        } 
+    });
+};
 
-                let addProduit = document.querySelector(".btn-primary");
-                console.log(addProduit);       
+makeRequest('GET', `https://oc-p5-api.herokuapp.com/api/furniture/${new URLSearchParams(window.location.search).get('id')}`)
+.then(function (produit) {
+   
+    produitHtml(produit);
 
-                addProduit.addEventListener("click", function (event) {
-                    event.preventDefault();
+    let select = document.querySelector("select");
+    let value = document.querySelector("option");
+    let produitColor = select.value;
+    let produitsChoosen = {
+            produitName: produit.name,
+            produitId: produit._id,
+            produitColor: select.value,
+            quantity: 1,
+            produitPrice: produit.price / 100,
+        };
+    console.log(produitsChoosen);
 
-                    let produitsChoosen = {
-                        produitproduitproduitName: produit.name,
-                        produitproduitId: produit._id,
-                        produitColor: select.value,
-                        quantity: 1,
-                        produitPrice: produit.price / 100,
-                    };
-                    console.log(produitsChoosen);
+    let storedProduits = JSON.parse(localStorage.getItem('newArticle'));
+    storedProduits.push(produitsChoosen);
+    localStorage.setItem('newArticle', JSON.stringify(storedProduits));
+    console.log(storedProduits);
 
-                    let storedProduits = JSON.parse(localStorage.getItem('newArticle'));
-                    const produitColor = select.value;
-                    if(storedProduits) {
-                        storedProduits.push(produitsChoosen);
-                        localStorage.setItem('newArticle', JSON.stringify(storedProduits));
-                        console.log(storedProduits);
-                        if (window.confirm(produit.name + " " + produitColor + ' a bien été ajouté. Souhaitez vous consulter votre panier ?')) { 
-                            window.location.href = "panier.html";
-                        } else {
-                            window.location.href = "index.html";
-                        }
-                    } 
-                });
-            } else {
-                console.error('Retour du serveur : ', response.status);
-                alert('Erreur rencontrée : ' + response.status);
-            } 
-        }
-        catch (error) {
-            alert("Erreur : " + error);
-        }
-    };
-    getProduits();
+    choosen(produit, produitColor, produitsChoosen, storedProduits);
 });
+
+
